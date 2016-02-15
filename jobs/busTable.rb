c57c4ffeb056198d3ee6782db9d32a46300de713
Johmpa <johmpa@gmail.com>
2016-02-15 23:13:06 +0100

    Remove Special Characters

--- jobs/busTable.rb
+++ jobs/busTable.rb
@@ -2,13 +2,13 @@ headers = [
     {cols: [
     {value: "Buss"},
     {value: "Mot"},
-    {value: "Åvgår"},
-    {value: "Störningar"}]
+    {value: "Departs"},
+    {value: "Disturbances"}]
 }
 ]
 
 rows = [{"cols"=>[{"value"=>"179"}, {"value"=>"Sollentuna Station"}, {"value"=>"08:32"}, {"value"=>"Inga"}]},
-        {"cols"=>[{"value"=>"179"}, {"value"=>"Sollentuna Station"}, {"value"=>"08:32"}, {"value"=>"Försenad pga köer"}]},
+        {"cols"=>[{"value"=>"179"}, {"value"=>"Sollentuna Station"}, {"value"=>"08:32"}, {"value"=>"Delayed due to Queues"}]},
         {"cols"=>[{"value"=>"Name 3"}, {"value"=>"Value 3"}]},
         {"cols"=>[{"value"=>"Name 4"}, {"value"=>"Value 4"}]}]
 SCHEDULER.every '1h' do
