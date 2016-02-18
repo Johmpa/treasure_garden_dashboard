fa9a75984070beca0f997c90a8487229b99c8acb
Johmpa <johmpa@gmail.com>
2016-02-18 23:32:07 +0100

    Continued on SL job, error may be due to file name conflicts

--- jobs/slTrafficsituation.rb
+++ jobs/slTrafficsituation.rb
@@ -10,9 +10,22 @@ SCHEDULER.every "15m", :first_in => 0 do |job|
   http     = Net::HTTP.new "api.sl.se"
   request  = http.request Net::HTTP::Get.new("/api2/trafficsituation.JSON?key=#{key}")
   response = JSON.parse request.body
-  puts response["ResponseData"]
+  #puts response["ResponseData"]
 
+  train = {}
+  metro = {}
   response["ResponseData"]["TrafficTypes"].each do |traffictype|
+    type = traffictype["Type"]
+    events = traffictype["Events"]
+    icon = traffictype["StatusIcon"]
 
+    if type == "train"
+      train = {StatusIcon: icon, Events: events}
+    end
+    if type == "metro"
+      metro = {StatusIcon: icon, Events: events}
+    end
   end
+  puts "Send Event..."
+  send_event "sltrafficsituation", {Train: train, Metro: metro}
 end
