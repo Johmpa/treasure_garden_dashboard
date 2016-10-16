a8b46a56f45f5b77a8ceab1b4c9ec98b19824f41
Johmpa <johmpa@gmail.com>
2016-10-16 12:51:49 +0200

    Revert till gammal reseplanerare pga frysningar vid jobbets slut

--- jobs/slreseplanerarework.rb
+++ jobs/slreseplanerarework.rb
@@ -7,10 +7,10 @@ $key = URI::encode('2ca36fc1304f4bc7ac9c87cf40b0dc06')
 #$uri             = URI.parse("http://api.sl.se/api2/TravelplannerV2/trip.JSON?key=#{key}&originId=9103&destId=9295&unsharp=1&numTrips=1&realtime=true")
 
 # 10000 anrop per månad har jag råd med -> drygt 13 anrop per timme
-#SCHEDULER.every "10m", :first_in => 0 do |job|
+SCHEDULER.every "6m", :first_in => 0 do |job|
 #SCHEDULER.cron '*/5 9-17 * * 1-5' do |job|
-SCHEDULER.cron '*/2 6-10 * * 1-5' do
-  puts "Sl Travel Plan: Frequent Calc"
+#SCHEDULER.cron '*/2 6-10 * * 1-5' do
+  puts "Sl Travel Plan: Calc"
   calculateTripAndSend
 end
 #SCHEDULER.cron '*/30 10-20 * * 1-5' do
